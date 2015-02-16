4dda669822f7ac5348396f8ba1ab6da7b68032b9
jamie7132 <jfsmills@umich.edu>
2015-02-16 04:05:13 -0500

    basic init screen

---
+++ Pitch Perfect/Pitch Perfect/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Jamie Sookprasong on 16/2/15.
+//  Copyright (c) 2015 Jamie Sookprasong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var recordingInProgress: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(sender: UIButton) {
+        recordingInProgress.hidden = false
+    }
+
+}
+
