4f05cf8872ff4f31d89565c9f1c31fa27328882e
jamie7132 <jfsmills@umich.edu>
2015-02-16 04:16:39 -0500

    added second view and segue

--- Pitch Perfect/Pitch Perfect/ViewController.swift
+++ Pitch Perfect/Pitch Perfect/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var recordingInProgress: UILabel!
     @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,11 +26,13 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(animated: Bool) {
         stopButton.hidden = true
+        recordButton.enabled = true
     }
 
     @IBAction func recordAudio(sender: UIButton) {
         recordingInProgress.hidden = false
         stopButton.hidden = false
+        recordButton.enabled = false
     }
 
     @IBAction func stopRecording(sender: UIButton) {
