b149d295b373f84006b74a75d4c6cb06c88fd2a6
jamie7132 <jfsmills@umich.edu>
2015-02-16 05:04:33 -0500

    reset sound file to start upon switching from slow to fast

--- Pitch Perfect/Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/Pitch Perfect/PlaySoundsViewController.swift
@@ -34,12 +34,14 @@ class PlaySoundsViewController: UIViewController {
     @IBAction func playSlowAudio(sender: UIButton) {
         audioPlayer.stop()
         audioPlayer.rate = 0.5
+        audioPlayer.currentTime = 0.0
         audioPlayer.play()
     }
 
     @IBAction func playFastAudio(sender: UIButton) {
         audioPlayer.stop()
         audioPlayer.rate = 2.0
+        audioPlayer.currentTime = 0.0
         audioPlayer.play()
     }
     
