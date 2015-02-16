c8ae12b33b7343bfbd3df08782d41e1780e633d6
jamie7132 <jfsmills@umich.edu>
2015-02-16 04:59:12 -0500

    added fast and stop for playing audio

--- Pitch Perfect/Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/Pitch Perfect/PlaySoundsViewController.swift
@@ -20,6 +20,7 @@ class PlaySoundsViewController: UIViewController {
         if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
             var filePathUrl = NSURL.fileURLWithPath(filePath)
             audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+            audioPlayer.enableRate = true
         } else {
             println("the filePath is empty")
         }
@@ -31,9 +32,21 @@ class PlaySoundsViewController: UIViewController {
     }
 
     @IBAction func playSlowAudio(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.rate = 0.5
         audioPlayer.play()
     }
 
+    @IBAction func playFastAudio(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.rate = 2.0
+        audioPlayer.play()
+    }
+    
+    @IBAction func stopAudio(sender: UIButton) {
+        audioPlayer.stop()
+    }
+    
     /*
     // MARK: - Navigation
 
