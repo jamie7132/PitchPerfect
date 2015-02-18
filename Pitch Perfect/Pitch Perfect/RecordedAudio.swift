fb26b6419d5565fc7b30756e30f6b23ad6a36892
jamie7132 <jfsmills@umich.edu>
2015-02-18 14:56:17 -0500

    implemented recording and use of user voice

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Jamie Sookprasong on 18/2/15.
+//  Copyright (c) 2015 Jamie Sookprasong. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject {
+    var filePathUrl: NSURL!
+    var title: String!
+}
